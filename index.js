c150a91a4f6e9892fddcd10daa4b49959f54a163
Christopher Bebry <invalidsyntax@gmail.com>
2014-02-13 00:11:17 -0500

    Added route for getting scores.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var Hapi = require('hapi'),
 	server = new Hapi.Server('0.0.0.0', 7676);
 
+var Scoreboard = require('./lib');
+
 server.route([
+	{
+		method: 'GET', path: '/games/{id}/scores', config: {
+			handler: function(request, reply) {
+				//var request = this;
+				var gameid = request.params.id;
+
+				Scoreboard.Games.getScores({gameid: gameid}, function(err, scores) {
+					if (err) throw err;
+					reply(scores);
+				})
+
+			}
+		}
+	},
 	{ method: '*', 	path: '/{path*}', handler: { directory: { path: './static/', listing: false, redirectToSlash: true } } }
 ]);
 server.start();
